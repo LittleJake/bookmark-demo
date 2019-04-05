e03d4241074cc702189e388f8ba1001507ab1c04
LittleJake <465917717@qq.com>
2019-04-05 23:46:12 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -7,30 +7,11 @@ class IndexController extends BaseController
     //推荐书签
     public function indexAction()
     {
-        dump($this->isLogin());
-        exit();
-        $rule = array(
-            array('email', 'email', '错误的邮箱', 1, 'unique', 1),
-            array('repass', 'pass', '两次密码不一致', 1, 'confirm', 1),
-            array('username', 'require', '账号已存在', 1, 'unique', 1)
-        );
-
-        $user = D('User');
+        if($this->isLogin())
 
 
-        $data['email'] = '5451212';
-        $data['username'] = '123123';
-        $data['pass'] = md5('123123');
-        $data['repass'] = md5('121233123');
-        if($user->validate($rule)->create($data, 1))
-        {
-            $user->data($data)->add();
-            echo '123';
-        }
-        else
-            exit($user->getError());
 
-        //return $this->display();
+        return $this->display();
     }
 
 }
--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -15,6 +15,28 @@ class UserController extends BaseController
     //登录
     public function loginAction()
     {
+        /*
+        $rule = array(
+            array('email', 'email', '错误的邮箱', 1, 'unique', 1),
+            array('repass', 'pass', '两次密码不一致', 1, 'confirm', 1),
+            array('username', 'require', '账号已存在', 1, 'unique', 1)
+        );
+
+        $user = D('User');
+
+
+        $data['email'] = '5451212';
+        $data['username'] = '123123';
+        $data['pass'] = md5('123123');
+        $data['repass'] = md5('121233123');
+        if($user->validate($rule)->create($data, 1))
+        {
+            $user->data($data)->add();
+            echo '123';
+        }
+        else
+            exit($user->getError());
+        */
         $this->assign('page_title', '登录');
         return $this->display();
     }
